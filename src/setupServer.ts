5f7f62d4311b643dec1a28d22d025b819b2fd6e0
littleFootDev <thelittlefootdev@gmail.com>
2022-02-28 23:05:04 +0100

    server is running

--- src/setupServer.ts
+++ src/setupServer.ts
@@ -1,23 +1,28 @@
-import Koa from 'koa';
-import koaCors from 'koa2-cors';
-import bodyParser from 'koa-bodyparser';
+import Koa from "koa";
+import koaCors from "koa2-cors";
+import bodyParser from "koa-bodyparser";
 
 async function serverSetup() {
-    const server : Koa = new Koa();
-    await startServer(server);
-};
-function middlewares() {};
-async function startServer(server : Koa){
-    try {
-        const port = 8080;
-        const serverStarted : Promise<void> = new Promise((resolve) => {
-            server.listen(port, resolve);
-        });
-        await serverStarted
-        console.log(`Serveur is running on port ${port}`);
-        
-    } catch (err) {
-        console.log(err);
-        return err;    
-    }
-};
+  const server: Koa = new Koa();
+  middlewares(server);
+  await startServer(server);
+}
+function middlewares(server : Koa) {
+    server.use(bodyParser());
+    server.use(koaCors());
+}
+async function startServer(server: Koa) {
+  try {
+    const port = 8080;
+    const serverStarted: Promise<void> = new Promise((resolve) => {
+      server.listen(port, resolve);
+    });
+    await serverStarted;
+    console.log(`Serveur is running on port ${port}`);
+  } catch (err) {
+    console.log(err);
+    return err;
+  }
+}
+
+export {serverSetup};
