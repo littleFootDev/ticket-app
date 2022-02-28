34453c5f1198dca7dfab34cf6cc8c2d21a645d59
littleFootDev <thelittlefootdev@gmail.com>
2022-02-28 22:43:58 +0100

    setupServer partie 1

---
+++ src/setupServer.ts
@@ -0,0 +1,23 @@
+import Koa from 'koa';
+import koaCors from 'koa2-cors';
+import bodyParser from 'koa-bodyparser';
+
+async function serverSetup() {
+    const server : Koa = new Koa();
+    await startServer(server);
+};
+function middlewares() {};
+async function startServer(server : Koa){
+    try {
+        const port = 8080;
+        const serverStarted : Promise<void> = new Promise((resolve) => {
+            server.listen(port, resolve);
+        });
+        await serverStarted
+        console.log(`Serveur is running on port ${port}`);
+        
+    } catch (err) {
+        console.log(err);
+        return err;    
+    }
+};
